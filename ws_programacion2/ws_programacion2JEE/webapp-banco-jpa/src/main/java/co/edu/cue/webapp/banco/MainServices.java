395795902c13e2e6ba8ddb41d134301f30097632
josalazar <53492638+devjosalazar@users.noreply.github.com>
2022-02-23 18:03:09 -0500

    actualización webapp-banco-jpa

--- ws_programacion2/ws_programacion2JEE/webapp-banco-jpa/src/main/java/co/edu/cue/webapp/banco/MainServices.java
+++ ws_programacion2/ws_programacion2JEE/webapp-banco-jpa/src/main/java/co/edu/cue/webapp/banco/MainServices.java
@@ -1,9 +1,9 @@
 package co.edu.cue.webapp.banco;
 
 import co.edu.cue.webapp.banco.entity.Cliente;
-import co.edu.cue.webapp.banco.entity.Cuenta;
-import co.edu.cue.webapp.banco.services.ClienteServiceImpl;
+import co.edu.cue.webapp.banco.exceptions.ClienteException;
 import co.edu.cue.webapp.banco.services.IClienteService;
+import co.edu.cue.webapp.banco.services.ClienteServiceImpl;
 import co.edu.cue.webapp.banco.util.DateUtil;
 import co.edu.cue.webapp.banco.util.JpaUtil;
 import jakarta.persistence.EntityManager;
@@ -14,66 +14,103 @@ import java.util.List;
 
 public class MainServices {
 
+
     public static void main(String[] args) {
 
         EntityManager em = JpaUtil.getEntityManager();
-
         IClienteService clienteService = new ClienteServiceImpl(em);
 
+        int idCliente = 1;
 
-        int idCliente = 4;
-        Cliente cliente = setDataCliente();
-
-        listarClientes(clienteService);
-        crearCliente(clienteService,cliente);
+        Cliente cliente = setDatosCliente();
 
         listarClientes(clienteService);
+        obtenerPorId(clienteService,idCliente);
 
-        //editarCliente(clienteService,idCliente);
-        //eliminarCliente(clienteService,idCliente);
+        listarClientes(clienteService);
+        insertarCliente(clienteService, cliente);
 
-
-        Cuenta cuenta = new Cuenta();
-
-        cuenta.setCliente(cliente);
-
+        listarClientes(clienteService);
+        editarCliente(clienteService, idCliente);
+        eliminarCliente(clienteService,idCliente);
         em.close();
     }
 
-    private static void crearCliente(IClienteService clienteService, Cliente cliente) {
+    private static void listarClientes(IClienteService clienteService) {
+        System.out.println("========== Listar clientes ==========");
+        List<Cliente> listaClientes = clienteService.listar();
+        imprimirListaClientes(listaClientes);
+    }
+
+    private static void obtenerPorId(IClienteService clienteService, int idCliente) {
+        System.out.println("========== Obtener por ID ==========");
+        Cliente cliente = clienteService.obtenerPorId(1);
+        System.out.println(cliente.toString());
+    }
+
+    private static void insertarCliente(IClienteService clienteService, Cliente cliente) {
+        System.out.println("========== Insertar Cliente ==========");
+        clienteService.guardar(cliente);
+        System.out.println("Cliente guardado con éxito");
+    }
+
+    private static void editarCliente(IClienteService clienteService, int idCliente ) {
+        System.out.println("========== Editar Cliente ==========");
+
+        Cliente cliente = null;
+        int id = Integer.valueOf(JOptionPane.showInputDialog("Ingrese el id del cliente a modificar:"));
+        cliente = clienteService.obtenerPorId(idCliente);
+        updateDatosClientes(cliente);
         clienteService.guardar(cliente);
     }
 
-    private static void listarClientes(IClienteService clienteService) {
-        List<Cliente> listaCientes = clienteService.listar();
-        imprimirListaClientes(listaCientes);
-    }
-
-    private static Cliente setDataCliente() {
+    private static void eliminarCliente(IClienteService clienteService, int idCliente) {
+        System.out.println("========== Eliminar Cliente ==========");
 
         Cliente cliente = null;
-        String nombre = JOptionPane.showInputDialog("Ingrese el nombre:");
-        String apellido = JOptionPane.showInputDialog("Ingrese el apellido:");
-        String cedula = JOptionPane.showInputDialog("Ingrese la cedula:");
-        String fechaNacimiento = JOptionPane.showInputDialog("Ingrese la fecha de nacimiento:");
-        int edad = Integer.valueOf(JOptionPane.showInputDialog("Ingrese la edad:"));
+        cliente = clienteService.obtenerPorId(idCliente);
+        clienteService.eliminar(cliente.getIdCliente());
 
-        cliente = new Cliente();
-        cliente.setNombre(nombre);
-        cliente.setApellido(apellido);
-        cliente.setCedula(cedula);
-
-        Date fechaNacimientoDate = DateUtil.formatearFecha(fechaNacimiento);
-        cliente.setFechaNacimiento(fechaNacimientoDate);
-        cliente.setEdad(edad);
-
-        return cliente;
     }
-
 
     private static void imprimirListaClientes(List<Cliente> listaClientes) {
         for (Cliente cliente : listaClientes) {
             System.out.println(cliente.toString());
         }
     }
+
+
+    private static Cliente setDatosCliente() {
+        Cliente cliente = new Cliente();
+        String nombre = "Juan alberto";
+        String apellido = "Arias perdomo";
+        String cedula = "890613";
+        String fechaNacimiento = "15-06-1995";
+        int edad = 25;
+        cliente.setNombre(nombre);
+        cliente.setApellido(apellido);
+        cliente.setCedula(cedula);
+        Date fechaNacimientoDate = DateUtil.formatearFecha(fechaNacimiento);
+        cliente.setFechaNacimiento(fechaNacimientoDate);
+        cliente.setEdad(edad);
+        return cliente;
+    }
+
+
+    private static void updateDatosClientes(Cliente cliente) {
+
+        String nombre = "Juan alberto";
+        String apellido = "Arias perdomo";
+        String cedula = "10949058888";
+        String fechaNacimiento = "11-06-1990";
+        int edad = 27;
+
+        cliente.setNombre(nombre);
+        cliente.setApellido(apellido);
+        cliente.setCedula(cedula);
+        Date fechaNacimientoDate = DateUtil.formatearFecha(fechaNacimiento);
+        cliente.setFechaNacimiento(fechaNacimientoDate);
+        cliente.setEdad(edad);
+    }
+
 }
--- ws_programacion2/ws_programacion2JEE/webapp-banco-jpa/src/main/java/co/edu/cue/webapp/banco/exceptions/ClienteException.java
+++ ws_programacion2/ws_programacion2JEE/webapp-banco-jpa/src/main/java/co/edu/cue/webapp/banco/exceptions/ClienteException.java
@@ -2,7 +2,6 @@ package co.edu.cue.webapp.banco.exceptions;
 
 public class ClienteException extends  Exception{
 
-
     public ClienteException(String mensaje) {
         super(mensaje);
     }
